8dcf6fc3f913486df493bf66e8b9a7c2078f5d0f
pschwagler <pschwagl@gmail.com>
2019-11-13 19:37:03 -0500

    fixed bug where recommended would not get updated in meta table

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./router");
+require("dotenv").config();
 const app = express();
 
 app.use(bodyParser.json());
