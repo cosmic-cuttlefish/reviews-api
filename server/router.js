d552eaebadbea69ad20523f255ecc598244e5586
pschwagler <pschwagl@gmail.com>
2019-11-18 11:44:34 -0500

    added server endpoint

--- server/router.js
+++ server/router.js
@@ -3,6 +3,7 @@ const { getReviewMeta } = require("./controllers/getReviewMeta");
 const postReview = require("./controllers/postReview");
 const putHelpful = require("./controllers/putHelpful");
 const putReport = require("./controllers/putReport");
+var os = require("os");
 const router = require("express").Router();
 
 router.get("/:productid/list", getReviewList);
@@ -14,5 +15,7 @@ router.get("/loaderio-86affb8d108df7e109e2250ca6f8f526", (req, res) => {
   res.send("loaderio-86affb8d108df7e109e2250ca6f8f526");
 });
 router.get("/check", (req, res) => res.sendStatus(200));
+
+router.get("/server", (req, res) => res.send(os.networkInterfaces()));
 
 module.exports = router;
